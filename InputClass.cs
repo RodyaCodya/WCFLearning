685e0f8c36a73037a5b36cc5d671883a334889f1
RodyaCodya <rodyacodya@gmail.com>
2017-08-10 21:29:04 +0300

    init

---
+++ InputClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyService
+{
+    public class InputClass
+    {
+        public double x;
+        public double y;
+    }
+}
